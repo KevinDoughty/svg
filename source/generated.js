fe24b9bb7e87029b53f58204fa3392f91f605001
Kevin Doughty <kvndy@protonmail.com>
2018-09-20 19:47:25 -0400

    Magic_8_MP3LwCMLFEAv.svg

--- source/generated.js
+++ source/generated.js
@@ -1,1 +1,1 @@
-export const files = ["Axidraw_Magic_3_MP3LAUFjHeJh_Fixed.svg", "Axidraw_Magic_4_MP3KRf3YPuwf_Fixed.svg", "Magic_5_MP3LwqdoNhdV.svg", "Magic_6_MP3LwCKYXFRK.svg", "Magic_7_MP3LySXEGG46.svg"];
+export const files = ["Axidraw_Magic_3_MP3LAUFjHeJh_Fixed.svg", "Axidraw_Magic_4_MP3KRf3YPuwf_Fixed.svg", "Magic_5_MP3LwqdoNhdV.svg", "Magic_6_MP3LwCKYXFRK.svg", "Magic_7_MP3LySXEGG46.svg", "Magic_8_MP3LwCMLFEAv.svg"];
